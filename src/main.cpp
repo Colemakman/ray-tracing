864fc3fe92ca76326fd361203d58aba72ba744b2
Alfie Baxter <34726358+Colemakman@users.noreply.github.com>
2025-06-01 23:42:29 +0100

    image

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,20 @@
-#include "color.h"
-#include "vec3.h"
+#include "rtweekend.h"
 
-#include <iostream>
+#include "camera.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 int main() {
-    
-    // Image
+    hittable_list world;
 
-    int image_width = 256;
-    int image_height = 256;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
-    // Render
+    camera cam;
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width  = 400;
 
-    for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
-            write_color(std::cout, pixel_color);
-        }
-    }
-
-    std::clog << "\rDone.                 \n";
+    cam.render(world);
 }
